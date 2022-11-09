600149cd698ba5e003a17f90ef5d0414a72e1b1a
Evgeniy Khaluev <ekhaluev@hsdigital.ru>
2022-11-09 23:05:16 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Gate struct {
+	Name  string
+	State []bool
+	Func  func([]bool) []bool
+}
+
+func (g *Gate) Eval(in []bool) (out []bool, changed bool) {
+	nextState := g.Func(in)
+	if len(g.State) != len(nextState) {
+		changed = true
+	} else {
+		for i := range g.State {
+			if nextState[i] != g.State[i] {
+				changed = true
+				break
+			}
+		}
+	}
+	copy(g.State, nextState)
+	return nextState, changed
+}
+
+func BufferFunc(in []bool) (out []bool) {
+	for _, v := range in {
+		out = append(out, v)
+	}
+	return out
+}
+
+func NotFunc(in []bool) (out []bool) {
+	for _, v := range in {
+		out = append(out, !v)
+	}
+	return out
+}
+
+func OrFunc(in []bool) (out []bool) {
+	res := in[0]
+	for _, v := range in {
+		res = res || v
+	}
+	return []bool{res}
+}
+
+func AndFunc(in []bool) (out []bool) {
+	res := in[0]
+	for _, v := range in[1:] {
+		res = res && v
+	}
+	return []bool{res}
+}
+
+func main() {
+	in := []bool{true, false}
+
+	buf := Gate{Name: "BUF", Func: BufferFunc}
+	not := Gate{Name: "NOT", Func: NotFunc}
+	and := Gate{Name: "AND", Func: AndFunc}
+	or := Gate{Name: "OR", Func: OrFunc}
+
+	queue := []Gate{buf, not, and, or}
+	for _, v := range queue {
+		val, _ := v.Eval(in)
+		fmt.Println("Gate:", v.Name, "in:", in, "out:", val)
+	}
+}
